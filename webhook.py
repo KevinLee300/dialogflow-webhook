96ae6804e618be030635a6abe837fe8b3cb93e60
KevinLee300 <a9503039@gmail.com>
2025-04-22 21:58:16 +0800

    Add files via upload

---
+++ webhook.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    req = request.get_json()
+    # 從 Dialogflow 中提取參數
+    spec_type = req.get("queryResult", {}).get("parameters", {}).get("spec_type", "")
+
+    if spec_type == "塑化":
+        reply = "這是塑化規範的下載連結：\nhttps://1drv.ms/b/c/c2f6a4a69f694f7a/ERTtlkWS33tJjZ4yg2-COYkBVv1DBbVmg0ui8plAduBb4A?e=AE5ybU"
+    elif spec_type == "企業":
+        reply = "這是企業規範的下載連結：\nhttps://1drv.ms/b/c/c2f6a4a69f694f7a/ERaG7Grpi7RLhLySygar-E0BqPzegJZTQK19aBUs01C55g?e=Bk6Cgz"
+    else:
+        reply = "請問是要查詢「塑化」還是「企業」規範？"
+
+    return jsonify({
+        "fulfillmentText": reply
+    })
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
